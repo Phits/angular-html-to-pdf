cfb10bf04c0493e6c3d70556fa00c073d1ed6283
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2023-01-11 08:59:10 -0500

    PDF creation working

--- htmltopdf/src/app/app.component.ts
+++ htmltopdf/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'htmltopdf';
+
+  @ViewChild('pdfTable') pdfTable: ElementRef | undefined;
+
+  //PDF generate button click function
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+
+    //get table html
+    if (this.pdfTable?.nativeElement) {
+      const pdfTable = this.pdfTable.nativeElement;
+      //html to pdf format
+      let html = htmlToPdfmake(pdfTable.innerHTML);
+
+      const documentDefinition = { content: html };
+      pdfMake.createPdf(documentDefinition).open();
+    }
+  }
+
 }
